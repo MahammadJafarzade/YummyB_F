9253fbea3d2c37c6e16f0de60c96da35c3a3d777
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-06-24 12:13:26 +0400

    Yummy

---
+++ Yummy/Yummy/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yummy.Models;
+
+namespace Yummy.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+    }
+}
